3581fa5dd2d30072e6072806a597d09dad433387
HealMon <71082215+RspSang@users.noreply.github.com>
2021-06-22 22:34:37 +0900

    study router

--- src/server.js
+++ src/server.js
@@ -1,27 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
-
-
-
-const Home = (req, res, next) => {
-  return res.send("I love middleware");
-  //return res.send("<h1>i still love you</h1>");
-  // next();
-};
-
-const Login = (req, res) => {
-  return res.send("Login here.")
-};
-
-
 app.use(logger);
 
-app.get("/", Home);
-app.get("/login", Login);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 
 const handleListening = (req, res, next) =>
   console.log(`Server listenting on port http://localhost:${PORT} 🚀`);
