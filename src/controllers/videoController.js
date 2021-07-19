9b9460fae8887b9d83a7ccc1dec772cf5a4d2e7c
HealMon <71082215+RspSang@users.noreply.github.com>
2021-07-19 22:27:51 +0900

    update edit page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,20 +8,39 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("404",{ pageTitle: "Video not found.",});
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Edit: ` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title} `, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const video = await Video.exists({_id:id});
+  const { title, description, hashtags } = req.body;
+
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
+
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 
