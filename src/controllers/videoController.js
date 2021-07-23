87461069274441ec8c132e6f55597a37647ebfc5
HealMon <71082215+RspSang@users.noreply.github.com>
2021-07-23 18:11:14 +0900

    make login/join user function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -18,7 +18,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.satatus(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title} `, video });
 };
@@ -28,7 +28,7 @@ export const postEdit = async (req, res) => {
   const video = await Video.exists({ _id: id });
   const { title, description, hashtags } = req.body;
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.satatus(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -51,7 +51,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.satatus(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
